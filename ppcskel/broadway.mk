3d29bd1a0458dd57b91e96a08cd24c3bbdec58f3
Andrew Piroli <45859969+AndrewPiroli@users.noreply.github.com>
2022-01-06 10:43:53 -0500

    Remove -mpaired CFLAG
    To align with default compiler settings in autobuild.sh script
    
    It's not that much of a speedup anyway, we can't expect people to run GCC 7.X and lower forever to accommodate this tiny feature.

--- ppcskel/broadway.mk
+++ ppcskel/broadway.mk
@@ -4,7 +4,7 @@ endif
 
 PREFIX = $(DEVKITAMATEUR)/bin/powerpc-none-elf-
 
-CFLAGS = -mcpu=750 -mpaired -m32 -mhard-float -mno-eabi -mno-sdata
+CFLAGS = -mcpu=750 -m32 -mhard-float -mno-eabi -mno-sdata
 CFLAGS += -ffreestanding -ffunction-sections
 CFLAGS += -Wall -Wextra -O0 -pipe
 ASFLAGS =
